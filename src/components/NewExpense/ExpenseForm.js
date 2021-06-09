64ace7e64b16a6599e544ea3679d0adbbafb487d
Caroline <caroline.jones@lumen.com>
2021-06-09 17:06:25 -0500

    Added state and event handlers to new expense form

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,21 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ExpenseForm.css'
 
 const ExpenseForm = () => {
+
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
+  const [enteredDate, setEnteredDate] = useState('');
+
+  const handleTitleChange = (event) => {
+    setEnteredTitle(event.target.value);
+  }
+  const handleAmountChange = (event) => {
+    setEnteredAmount(event.target.value);
+  }
+  const handleDateChange = (event) => {
+    setEnteredDate(event.target.value);
+  }
 
   return (
     <div className="new-expense__controls">
       <div className="new-expense__control">
         <label>Title</label>
-        <input type="text" />
+        <input type="text" onChange={handleTitleChange}/>
       </div>
       <div className="new-expense__control">
         <label>Amount</label>
-        <input type="number" min=".01" step=".01"/>
+        <input type="number" onChange={handleAmountChange} min=".01" step=".01"/>
       </div>
       <div className="new-expense__control">
         <label>Date</label>
-        <input type="date" min="2019-01-01" max="2022-12-31" />
+        <input type="date" onChange={handleDateChange} min="2019-01-01" max="2022-12-31" />
       </div>
       <div className="new-expense__actions">
         <button type="submit">Add Expense</button>
